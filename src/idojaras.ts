6bc571e5aaf5a263148641dc1e746bd00e1578c1
BraunRoland <braun.roland@petrik.hu>
2026-09-21 08:30:24 +0200

    adatBeolvasas

---
+++ src/idojaras.ts
@@ -0,0 +1,4 @@
+export interface Idojaras {
+    day: string,
+    temperature: number
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
+import type { Idojaras } from './idojaras'
 import './style.css'
+const idojarasok: Idojaras[] = []
+
+async function adatBeolvasas() {
+  const response = await fetch('https://petrik-idojaras-default-rtdb.europe-west1.firebasedatabase.app/.json')
+  if(!response.ok) {
+    throw new Error('hiba')
+  }
+  else {
+    const data = await response.json();
+    data.forEach((element: Idojaras) => {
+      idojarasok.push(element);
+    });
+  }
+}
+
+async function init() {
+  await adatBeolvasas();
+  console.log(idojarasok);
+}
+
+document.addEventListener('DOMContentLoaded', init)
