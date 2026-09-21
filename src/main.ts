9be23a0584201d9f8be80aea5be4a8b7d2c762b4
BraunRoland <braun.roland@petrik.hu>
2026-09-21 08:38:04 +0200

    kiiras

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import type { Idojaras } from './idojaras'
 import './style.css'
-const idojarasok: Idojaras[] = []
+let idojarasok: Idojaras[] = []
 
 async function adatBeolvasas() {
   const response = await fetch('https://petrik-idojaras-default-rtdb.europe-west1.firebasedatabase.app/.json')
@@ -15,8 +15,24 @@ async function adatBeolvasas() {
   }
 }
 
+function kiiras() {
+  const table = document.getElementById('kiiras') as HTMLTableElement;
+  idojarasok.forEach((item:Idojaras) => {
+    const row = document.createElement('tr') as HTMLTableRowElement;
+    const day = document.createElement('td') as HTMLTableCellElement;
+    const temp = document.createElement('td') as HTMLTableCellElement;
+    day.innerText = item.day;
+    temp.innerText = item.temperature.toLocaleString();
+    row.appendChild(day);
+    row.appendChild(temp);
+    table.appendChild(row);
+  })
+}
+
 async function init() {
+  idojarasok = [];
   await adatBeolvasas();
+  kiiras();
   console.log(idojarasok);
 }
 
